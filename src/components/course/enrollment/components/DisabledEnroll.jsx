5b8d9cdc98b564cc7b34d738c3855c403786b403
LokeshSajjanapu-kognoz <Lokesh.Sajjanapu@kognoz.ai>
2021-09-13 11:59:44 +0530

    thirteenth commit

--- src/components/course/enrollment/components/DisabledEnroll.jsx
+++ src/components/course/enrollment/components/DisabledEnroll.jsx
@@ -1,19 +1,12 @@
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
 import { AppContext } from '@edx/frontend-platform/react';
 import { CourseContext } from '../../CourseContextProvider';
 
 const EnrollBtnDisabled = () => {
-
   const { enterpriseConfig } = useContext(AppContext);
   const { state: courseData } = useContext(CourseContext);
   return (
     <a href={`${process.env.LMS_BASE_URL}/enterprise/${enterpriseConfig.uuid}/course/${courseData.activeCourseRun.key}/enroll`}>Enroll Now</a>
   );
 };
-
-EnrollBtnDisabled.propTypes = {
-  enrollLabel: PropTypes.node.isRequired,
-}
-
 export default EnrollBtnDisabled;
