bfd0f54f6cb5acc36175bc2d0959ed115355bc92
LokeshSajjanapu-kognoz <Lokesh.Sajjanapu@kognoz.ai>
2021-09-13 10:38:47 +0530

    second commit

--- src/components/course/enrollment/components/DisabledEnroll.jsx
+++ src/components/course/enrollment/components/DisabledEnroll.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import PropTypes from 'prop-types';
 
 import { EnrollButtonCta } from '../common';
+import { AppContext } from '@edx/frontend-platform/react';
+import { CourseContext } from '/home/azureuser/apps/openedx/frontend-app-learner-portal-enterprise/src/components/course/CourseContextProvider'
 
-const EnrollBtnDisabled = ({ enrollLabel }) => (
-  <EnrollButtonCta
-    enrollLabel={enrollLabel}
-    as="div"
-    className="btn btn-light btn-block disabled d-block"
-  />
+const EnrollBtnDisabled = ({ enrollLabel }) => {
+     
+  const { enterpriseConfig } = useContext(AppContext);
+  console.log(enterpriseConfig);
+  console.log(enterpriseConfig.uuid)
+  const { state: courseData } = useContext(CourseContext);
+  console.log(courseData);
+ console.log(courseData.activeCourseRun.key);
+ 
+  return (
+     <a href= {`${process.env.LMS_BASE_URL}/enterprise/${enterpriseConfig.uuid}/course/${courseData.activeCourseRun.key}/enroll`}>Enroll Now</a>
 );
+}
 
 EnrollBtnDisabled.propTypes = {
   enrollLabel: PropTypes.node.isRequired,
