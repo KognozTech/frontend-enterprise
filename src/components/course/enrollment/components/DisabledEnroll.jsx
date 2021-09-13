8e49fef0fa9198add4ffb556f12e6ceb1af7dafe
LokeshSajjanapu-kognoz <Lokesh.Sajjanapu@kognoz.ai>
2021-09-13 11:03:35 +0530

    fourth commit

--- src/components/course/enrollment/components/DisabledEnroll.jsx
+++ src/components/course/enrollment/components/DisabledEnroll.jsx
@@ -1,18 +1,13 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { AppContext } from '@edx/frontend-platform/react';
-import { CourseContext } from '../../CourseContextProvider.jsx'
+import { CourseContext } from '../../CourseContextProvider.jsx';
 
 const EnrollBtnDisabled = () => {
   const { enterpriseConfig } = useContext(AppContext);
   const { state: courseData } = useContext(CourseContext);
   return (
-     <a href= {`${process.env.LMS_BASE_URL}/enterprise/${enterpriseConfig.uuid}/course/${courseData.activeCourseRun.key}/enroll`}>Enroll Now</a>
-);
+    <a href={`${process.env.LMS_BASE_URL}/enterprise/${enterpriseConfig.uuid}/course/${courseData.activeCourseRun.key}/enroll`}>Enroll Now</a>
+  );
 }
-
-EnrollBtnDisabled.propTypes = {
-  enrollLabel: PropTypes.node.isRequired,
-};
-
 export default EnrollBtnDisabled;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,1 +1,1 @@
-export { default as DashboardSidebar } from './DashboardSidebar';
+export { default as DashboardSidebar } from './sidebar/DashboardSidebar';
